ecd0f9b595505a4fedfd9af4d744d71b8f6cdc1f
applecheck1 <andreajbecerra2@gmail.com>
2024-07-03 22:13:13 -0500

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,132 @@
+const productos = {
+    "celulares": [
+        {
+            "nombre": "Iphone 12 original",
+            "imagen": "celulares/i12.webp",
+            "precio": 2000000
+        }
+    ],
+    "cargadores": [
+        {
+            "nombre": "Cabeza de cargador 5W, 1A entrada USB",
+            "imagen": "cabezas/1.jpeg",
+            "precio": 5000 
+        },  
+    ],
+    "cables":[
+        {
+            "nombre": "Cable de datos para Iphone 2.4 A Longitut 1m",
+            "imagen": "cables/2.jpeg",
+            "precio": 8000 
+        },  
+        {
+            "nombre": "Cable de datos V-8 2.4 A Longitut 1m",
+            "imagen": "cables/1.jpeg",
+            "precio": 8000
+        },
+        {
+            "nombre": "Cable de datos tipo C 2.4 A Longitut 1m",
+            "imagen": "cables/3.jpeg",
+            "precio": 8000 
+        },     
+    ],
+    "cabezas":[
+        {
+            "nombre": "Cabeza de cargador 5W, 1A entrada USB",
+            "imagen": "cabezas/1.jpeg",
+            "precio": 5000 
+        },  
+        {
+            "nombre": "Cabeza de cargador 5W, 1A entrada USB",
+            "imagen": "cabezas/2.jpeg",
+            "precio": 5000 
+        },  
+    ],
+    "audifonos":[
+        {
+            "nombre": "Audifonos manos libres, excelente calidad",
+            "imagen": "audifonos/1.jpeg",
+            "precio": 5000 
+        },  
+        {
+            "nombre": "Airpods Pro 5 de bluetooth para celular",
+            "imagen": "audifonos/2.jpeg",
+            "precio": 50000 
+        },
+    ],
+    "forros-iphone":[
+        {
+            "nombre": "Forros Xs max ",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+        {
+            "nombre": "Forros 11",
+            "imagen": "forros",
+            "precio": 10000 
+        },
+        {
+            "nombre": "Forros 11 Pro ",
+            "imagen": "forros",
+            "precio": 10000 
+        },
+        {
+            "nombre": "Forros 12-12 Pro ",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+        {
+            "nombre": "Forros 13 Pro-14 Pro",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+        {
+            "nombre": "Forros 13 Pro Max ",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+        {
+            "nombre": "Forros",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+        {
+            "nombre": "Forros",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+        {
+            "nombre": "Forros",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+    ],
+    "forros-samsung":[
+        {
+            "nombre": "Forros",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+    ],
+    "forros-xiaomi":[
+        {
+            "nombre": "Forros",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+    ],
+    "forros-motorola":[
+        {
+            "nombre": "Forros 8GPLUS",
+            "imagen": "forros/M1.jpeg",
+            "precio": 10000 
+        },  
+    ],
+    "forros-huawei":[
+        {
+            "nombre": "Forros",
+            "imagen": "forros",
+            "precio": 10000 
+        },  
+    ]
+};
---
+++ script3.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const productosContainer = document.getElementById("productos-container");
+    // Iterar sobre los productos de la categoría "cargadores"
+    productos["cabezas"].forEach(producto => {
+        const productoElement = document.createElement("div");
+        productoElement.classList.add("col-md-6", "col-lg-4", "mb-1");
+        productoElement.innerHTML = `
+            <div class="card">
+                <img src="${producto.imagen}" class="card-img-top" alt="Imagen de ${producto.nombre}">
+                <div class="card-body">
+                    <h5 class="card-title font-weight-bold mb-1">${producto.nombre}</h5>
+                    <p class="precio">Precio: $${producto.precio.toFixed(2)}</p>
+                </div>
+            </div>
+        `;
+
+        productosContainer.appendChild(productoElement);
+    });
+});
